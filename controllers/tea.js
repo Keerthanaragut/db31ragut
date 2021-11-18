36b8d530247746eadfd3850adbc2e6a8585e0996
Keerthanaragut <keerthanaragut@gmail.com>
2021-11-17 22:21:03 -0600

    bonus

--- controllers/tea.js
+++ controllers/tea.js
@@ -59,3 +59,105 @@ exports.tea_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // for a specific tea. 
+    exports.tea_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await tea.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+ 
+//Handle tea update form on PUT. 
+exports.tea_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await tea.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Flavor) toUpdate.Flavor = req.body.Flavor; 
+        if(req.body.color) toUpdate.color = req.body.color; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+
+// Handle tea delete on DELETE. 
+exports.tea_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await tea.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+// Handle a show one view with id specified by query 
+exports.tea_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await tea.findById( req.query.id) 
+        res.render('teadetail',  
+{ title: 'tea Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for creating a tea. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.tea_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('teacreate', { title: 'tea Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a tea. 
+// query provides the id 
+exports.tea_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await tea.findById(req.query.id) 
+        res.render('teaupdate', { title: 'tea Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.tea_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await tea.findById(req.query.id) 
+        res.render('teadelete', { title: 'tea Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
--- routes/tea.js
+++ routes/tea.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'); 
+const tea_controlers= require('../controllers/tea'); 
+var router = express.Router(); 
+ 
+/* GET teas */ 
+router.get('/', tea_controlers.tea_view_all_Page ); 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('tea', { title: 'Express' });
-});
 
-module.exports = router;
+// GET request for one tea. 
+router.get('/teas/:id', tea_controlers.tea_detail); 
+
+/* GET detail tea page */ 
+router.get('/detail', tea_controlers.tea_view_one_Page);
+
+/* GET create tea page */ 
+router.get('/create', tea_controlers.tea_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', tea_controlers.tea_update_Page); 
+
+/* GET delete tea page */ 
+router.get('/delete', tea_controlers.tea_delete_Page); 
+
+
+
+module.exports = router; 
